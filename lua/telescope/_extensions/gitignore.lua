cb2b6da0fbda081da6f13d46b5cb143076bce6a6
artumont <197291181+artumont@users.noreply.github.com>
2026-06-13 22:16:03 -0600

    feat: add Telescope extension, user commands, and documentation for gitignore-templates.nvim

---
+++ lua/telescope/_extensions/gitignore.lua
@@ -0,0 +1,5 @@
+--- Telescope extension loader for gitignore-templates.nvim
+--- This file is discovered automatically by telescope.nvim when
+--- calling require("telescope").load_extension("gitignore")
+
+return require("gitignore-templates.telescope")
